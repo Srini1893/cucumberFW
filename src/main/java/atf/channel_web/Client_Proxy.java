5c8b50019d6be5f55cd7a5e6b647cd6159d83d5a
Srini1893 <srinivasan@gmail.com>
2021-05-04 12:12:51 +0530

    my 2nd commit

--- src/main/java/atf/channel_web/Client_Proxy.java
+++ src/main/java/atf/channel_web/Client_Proxy.java
@@ -1,8 +1,22 @@
 package atf.channel_web;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import atf.Parent_proxy;
+import atf.start.Start_Web;
 import cucumber.api.Scenario;
 
 public class Client_Proxy extends Parent_proxy{
@@ -15,5 +29,83 @@ public class Client_Proxy extends Parent_proxy{
 		launch_browser();
 		Start_Web.browser.manage().window.maximize();
 		Start_Web.browser.manage().timeouts.implicitlyWait(timeout, TimeUnit.SECONDS);
+	}
+	
+	public void attachScreenShot() {
+		final byte[] screenshot = (Start_Web.browser).getScreenshotAs(OutputType.BYTES);
+		scenario.embed(screenshot, "image/png");
+	}
+	
+	public void printText(String str) {
+		scenario.write(str);
+	}
+	
+	public void launch_browser() {
+		load_properties(true);
+		initiate_browser();
+		Start_Web.browser.manage().window().maximize();
+	}
+	
+	private void initiate_browser() {
+		Driver_Loader path = new Driver_Loader();
+		WebDriver driver;
+		String browser = "webdriver." + prop.getProperty("BROWSER") + ".driver";
+		System.setProperty(browser, String.valueOf(path.PATH_TO_DRIVER));
+		if(prop.getProperty("BROWSER").equals("chrome")) {
+			driver = new ChromeDriver(set_caps());
+			
+		}else {
+			driver = new InternetExplorerDriver(set_caps();)
+		}
+		event_listener(driver);
+	}
+	
+	private DesiredCapabilities set_caps() {
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		LoggingPreferences logPrefs = new LoggingPreferences();
+		logPrefs.enable(LogType.BROWSER, Level.ALL);
+		caps.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+		ChromeOptions option = new ChromeOptions();
+		option.setBinary("chrome.exe path from program files folder");
+		caps.setCapability(ChromeOptions.CAPABILITY, option);
+		return caps;
+	}
+	
+	public void clear_session() {
+		Start_Web.browser.get("javascript:localStorage.clear();");
+		clear_browser_cache();
+	}
+	
+	private void clear_session_storage() {
+		// clear local storage
+
+	}
+	
+	private void clear_browser_cache() {
+		Start_Web.browser.manage().deleteAllCookies();
+
+	}
+	
+	private void event_listener(WebDriver driver) {
+		Start_Web.browser = new EventFiringWebDriver(driver);
+		WebEventListener eventListener = new WebEventListener();
+		if(prop.get("LOG")).toString().contains("true")){
+			Start_Web.browser.register(eventListener);
+		}else {
+			Start_Web.browser.unregister(eventListener);
+		}
+
+	}
+	
+	public void quit_browser() {
+		Start_Web.browser.quit();
+	}
+	
+	public void close_browser() {
+		Set<String> windowHandles = Start_Web.browser.getWindowHandles();
+		for(String window : windowHandles) {
+			Start_Web.browser.switchTo().window(window);
+			Start_Web.browser.close();
+		}
 	}
 }
--- src/main/java/atf/start/Start_Web.java
+++ src/main/java/atf/start/Start_Web.java
@@ -1,5 +1,94 @@
 package atf.start;
 
-public class Start_Web {
+
+import atf.Logging;
+import atf.channel_web.Client_Proxy;
+import atf.start.helper.helper;
+import cucumber.api.Scenario;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+
+
+public class Start_Web extends helper{
+
+	public static final int DEFAULT_TIMEOUT =45;
+	public static EventFiringWebDriver browser;
+	private Client_Proxy client_proxy;
+
+	public Start_Web(Scenario scenario) {
+		client_proxy = new Client_Proxy();
+		client_proxy.initialize(scenario);
+		start_log(scenario);
+	}
+
+	public void attachScreenshot() {
+		client_proxy.attachScreenShot();
+	}
+
+	public void printText(String str) {
+		client_proxy.printText(str);
+	}
+
+	public void close_browser(Scenario scenario) {
+		client_proxy.close_browser();
+		client_proxy.quit_browser();
+		close_log(scenario);
+
+	}
+
+	public void log_file_handler() {
+		Logging.merge_to_single_log_file();
+		Logging.delete_temp_dir();
+	}
+
+	private void close_log(Scenario scenario) {
+		logging.end_log("Finishing scenario >>>>" + scenario.getName() + "\n") +
+		StringUtils.repeat("*", 120) + "\n");
+
+	}
+
+	private void start_log(Scenario scenario) {
+		logging.start_log("Starting scenario >>>>"+ scenario.getName());
+
+	}
+
+	public Object load_class(Path path) {
+		File file = new File(String.valueOf(path));
+
+		URL url = null;
+		try {
+			url = file.toURI().toURL();
+		}catch(MalformedURLException e) {
+			e.printStackTrace();
+		}
+		URL[] urls = new URL[] {url};
+		ClassLoader cl = new URLClassLoader(urls);
+
+		Class cls = null;
+
+		try {
+			cls = cl.loadClass("Constants");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		Object object = null;
+		try {
+			object = cls.newInstance();
+		}catch (InstantiationException e) {
+			e.printStackTrace();
+		}catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return object;
+	}
+
 
 }
